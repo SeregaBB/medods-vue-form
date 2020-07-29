e3de2d7f494eda567bd95ffc0dcd052117b80161
Sergey <sergey@MacBook-Pro-Lyudmila.local>
2020-07-29 11:47:54 +0300

    make form component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,51 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <RegisterForm :inputs="inputs" />
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import RegisterForm from "./components/RegisterForm.vue";
 
 export default {
   name: "App",
+  data: () => { //дата должна быть функцией 
+    return {
+
+      inputs: { 
+        personal: {
+          //типы и плейсхолдеры для инпутов
+        0: {type: 'email', plholder: 'Фамилия', isRequired: true}, 
+        1: {type: 'alphaNum', plholder: 'Имя', isRequired: true}, 
+        2: {type: 'alphaNum', plholder: 'Отчество', isRequired: false}, 
+        3: {type: 'date', plholder: 'Дата рождения', isRequired: true},
+        4: {type: 'tel', plholder: '+7(000)00-00-00', isRequired: true},  
+        5: {type: 'alphaNum', plholder: 'Пол', isRequired: false}, //мультиселект
+        6: {type: 'alphaNum', plholder: 'Группа пациентов', isRequired: true},  //заменить на мультиселект, подумать как это сделать во Вью
+        7: {type: 'alphaNum', plholder: 'Лечащий врач', isRequired: false}, //Selector 
+        8: {type: 'checkbox', plholder: '', isRequired: false},  
+        },
+        adress: {
+        9: {type: 'number', plholder: 'Индекс', isRequired: false},
+        10: {type: 'alphaNum', plholder: 'Страна', isRequired: false},
+        11: {type: 'alphaNum', plholder: 'Область', isRequired: false},
+        12: {type: 'alphaNum', plholder: 'Город', isRequired: true},
+        13: {type: 'alphaNum', plholder: 'Улица', isRequired: false},
+        14: {type: 'alphaNum', plholder: 'Дом', isRequired: false},
+        },
+        documents: {
+        15: {type: 'alphaNum', plholder: 'Тип документа', isRequired: true},//selector
+        16: {type: 'number', plholder: 'Серия', isRequired: false},
+        17: {type: 'number', plholder: 'Номер', isRequired: false},
+        18: {type: 'alphaNum', plholder: 'Кем выдан', isRequired: false},
+        19: {type: 'date', plholder: 'дата выдачи', isRequired: true},
+        }
+      }
+    };
+  },
   components: {
-    HelloWorld
-  }
+    RegisterForm
+  },
 };
 </script>
 
@@ -22,7 +55,13 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color: #15202b;
+  margin: 0;
+  width: 100vw;
+  background-image: url('./assets/back.jpg');
+  padding: 20px 0;
+}
+body{ 
+  margin: 0
 }
 </style>
---
+++ src/components/RegisterForm.vue
@@ -0,0 +1,232 @@
+<template>
+  <div class="register">
+    <form class="main_form" @input="onInp" @click="onInp">
+      <span class="form_block_title">Персональная информация</span>
+      <input
+        id="0"
+        type="text"
+        placeholder="Фамилия (Обязательно)"
+        v-model.trim="lastName"
+        @input="$v.lastName.$touch()"
+        :class="{invalid: ($v.lastName.$error)}"
+      />
+      <small class="error" v-if="!$v.lastName.minLength">Фамилия не может состоять из 1 буквы</small>
+      <small class="error" v-else-if="!$v.lastName.alpha">Фамилия должна состоять только из букв</small>
+      <small class="error" v-else-if="$v.lastName.$dirty && !$v.lastName.required">Введите фамилию</small>
+
+      <input
+        id="1"
+        type="text"
+        placeholder="Имя (Обязательно)"
+        v-model.trim="name"
+        @input="$v.name.$touch()"
+        :class="{invalid: ($v.name.$error)}"
+      />
+
+      <small class="error" v-if="!$v.name.minLength">Имя не может состоять из 1 буквы</small>
+      <small class="error" v-else-if="!$v.name.alpha">Имя должно состоять только из букв</small>
+      <small class="error" v-else-if="$v.name.$dirty && !$v.name.required">Введите имя</small>
+
+      <input id="2" type="text" placeholder="Отчество " />
+      <input id="3" type="date" placeholder="Дата рождения (Обязательно)" />
+      <input id="4" type="tel" placeholder="+7(000)00-00-00 (Обязательно)" />
+      <input id="5" type="text" placeholder="Пол " />
+      <!--Это должен быть мультиселект-->
+      <select multiple
+        v-model="group"
+        @click="$v.group.$touch()"
+        :class="{invalid: ($v.group.$dirty && $v.group.$model === 'Группа пациентов')}"
+      >
+        <option disabled >Группа пациентов</option>
+        <option>Vip</option>
+        <option>Проблемные</option>
+        <option>ОМС</option>
+      </select>
+
+      <small
+        class="error"
+        v-if="$v.group.$dirty && $v.group.$model === 'Группа пациентов'"
+      >Выберите группу пациентов</small>
+
+      <select
+        v-model="doctor"
+        @click="$v.doctor.$touch()"
+        :class="{invalid: ($v.doctor.$dirty && $v.doctor.$model === 'Лечащий врач')}"
+      >
+        <option disabled >Лечащий врач</option>
+        <option>Иванов</option>
+        <option>Захаров</option>
+        <option>Чернышева</option>
+      </select>
+
+      <small
+        class="error"
+        v-if="$v.doctor.$dirty && $v.doctor.$model === 'Лечащий врач'"
+      >Выберите врача</small>
+
+
+      <div class="group">
+        <input id="8" name="sms" type="checkbox" v-model="sms" />
+        <label v-if="!$v.sms.$model" for="sms">Не отправлять мне СМС</label>
+        <label v-else for="sms">СМС не будут приходить</label>
+      </div>
+
+      <span class="form_block_title">Адрес</span>
+      <input type="number" placeholder="Индекс " />
+      <input type="text" placeholder="Страна " />
+      <input type="text" placeholder="Область " />
+
+      <input
+        type="text"
+        placeholder="Город (Обязательно)"
+        v-model.trim="city"
+        @input="$v.city.$touch()"
+        :class="{invalid: ($v.city.$error)}"
+      />
+      <small
+        class="error"
+        v-if="!$v.city.minLength"
+      >Название города не может состоять из менее, чем 3 букв</small>
+      <small class="error" v-else-if="!$v.city.alpha">Название города должно состоять только из букв</small>
+      <small class="error" v-else-if="$v.city.$dirty && !$v.city.required">Введите название города</small>
+
+      <input type="text" placeholder="Улица " />
+      <input type="text" placeholder="Дом " />
+      <span class="form_block_title">Документ</span>
+
+      <input type="text" placeholder="Тип документа (Обязательно)" />
+      <select
+        v-model="doctype"
+        @click="$v.doctype.$touch()"
+        :class="{invalid: ($v.doctype.$dirty && $v.doctype.$model === 'Тип документа')}"
+      >
+        <option disabled >Тип документа</option>
+        <option>Паспорт</option>
+        <option>Свидетельство о рождении</option>
+        <option>Вод. удостоверение</option>
+      </select>
+      <small
+        class="error"
+        v-if="$v.doctype.$dirty && $v.doctype.$model === 'Тип документа'"
+      >Выберите тип документа</small>
+
+      <input type="number" placeholder="Серия " />
+      <input type="number" placeholder="Номер " />
+      <input type="text" placeholder="Кем выдан " />
+      <input type="date" placeholder="дата выдачи (Обязательно)" />
+      <button type="button" v-if="this.$v.$anyError" disabled>Сохранить</button>
+      <button type="button" v-else>Сохранить</button>
+    </form>
+  </div>
+</template>
+
+<script>
+import { email, helpers, required, minLength } from "vuelidate/lib/validators";
+const alpha = helpers.regex("alpha", /^[а-яА-Яa-zA-Z]*$/);
+
+export default {
+  name: "RegisterForm",
+  //props: ["inputs"],
+  ///vuelidate
+
+  data: () => ({
+    lastName: "",
+    name: "",
+    birthday: "",
+    tel: "",
+    group: ["Группа пациентов"],
+    city: "",
+    doctor: "Лечащий врач",
+    doctype: "Тип документа",
+    docdate: "",
+    sms: false
+  }),
+  validations: {
+    email: { email, required },
+    lastName: { alpha, required, minLength: minLength(2) },
+    name: { alpha, required, minLength: minLength(2) },
+    group: { required },
+    doctor: { required },
+    sms: false,
+    city: { required, minLength: minLength(3), alpha },
+    doctype: {required}
+  },
+
+  ///методы
+  methods: {
+    onInp() {
+      console.log(this.$v);
+    }
+  }
+  //конец методов
+};
+</script>
+
+
+<style scoped lang="scss">
+.main_form {
+  width: 500px;
+  margin: auto;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background: white;
+  padding: 10px 40px;
+  border-radius: 15px;
+}
+
+.main_form {
+  input,
+  select {
+    margin: 0 0 10px;
+    padding: 10px;
+    border: none;
+    outline: none;
+    border-radius: 10px;
+    font-weight: bolder;
+    box-shadow: 0 4px 5px #ccc;
+    &.invalid {
+      box-shadow: 0 4px 5px rgb(255, 129, 129);
+    }
+  }
+  small {
+    color: rgb(255, 129, 129);
+    align-self: start;
+    font-weight: bold;
+  }
+  button {
+    margin: 0 0 10px;
+    padding: 10px;
+    border: none;
+    outline: none;
+    border-radius: 10px;
+    font-weight: bolder;
+    box-shadow: 0 4px 5px #ccc;
+    background: yellowgreen;
+    cursor: pointer;
+    &:hover {
+      transition: 0.4s;
+      background: rgb(102, 165, 2);
+    }
+    &:active {
+      background: rgb(67, 105, 1);
+    }
+    &:disabled {
+      cursor: not-allowed;
+    }
+  }
+
+  .group {
+    align-self: start;
+    label {
+      margin: 0 0 0 5px;
+    }
+  }
+}
+
+span {
+  font-size: 30px;
+  font-weight: bolder;
+  text-shadow: 0 3px 3px white;
+}
+</style>
